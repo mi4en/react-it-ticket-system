695ebf37f6632cbe81a9cce0d7be897f07d9a32c
Mario <marionikolov87@gmail.com>
2021-04-22 07:50:34 +0300

    setup store;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
@@ -18,17 +20,19 @@ const App = () => {
 	});
 
 	return (
-		<Fragment>
-			<SearchBar />
-			<div className='container'>
-				<AddBtn />
-				<AddLogModal />
-				<EditLogModal />
-				<AddTechModal />
-				<TechListModal />
-				<Logs />
-			</div>
-		</Fragment>
+		<Provider store={store}>
+			<Fragment>
+				<SearchBar />
+				<div className='container'>
+					<AddBtn />
+					<AddLogModal />
+					<EditLogModal />
+					<AddTechModal />
+					<TechListModal />
+					<Logs />
+				</div>
+			</Fragment>
+		</Provider>
 	);
 };
 
