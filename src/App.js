7b1df26189d328c666f2742ccd47d6e9ba0a0264
Mario <marionikolov87@gmail.com>
2021-04-22 07:22:35 +0300

    add more modals;

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
 	// Init Materialize JS
@@ -20,6 +23,9 @@ const App = () => {
 			<div className='container'>
 				<AddBtn />
 				<AddLogModal />
+				<EditLogModal />
+				<AddTechModal />
+				<TechListModal />
 				<Logs />
 			</div>
 		</Fragment>
