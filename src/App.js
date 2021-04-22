0ac21b69e2ca3c4dde4a3cf3f96124576adde354
Mario <marionikolov87@gmail.com>
2021-04-22 06:54:18 +0300

    log modal; add btn;

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+
 import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = () => {
 	// Init Materialize JS
@@ -15,6 +18,8 @@ const App = () => {
 		<Fragment>
 			<SearchBar />
 			<div className='container'>
+				<AddBtn />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</Fragment>
